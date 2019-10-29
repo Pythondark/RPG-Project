daa132f06940df8c77a2e32ec8417126a5296d9f
jacoblightspeedsolutions <jacob@lightspeedsolutions.ca>
2019-10-28 18:49:57 -0700

    Initial Commit

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,58 @@
+﻿using RPG.Core;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour, IAction
+    {
+        //[SerializeField] GameObject target;
+
+        NavMeshAgent navMeshAgent;
+        Health health;
+        //ActionScheduler actionScheduler;
+
+        private void Start()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+            health = GetComponent<Health>();
+        }
+
+        void Update()
+        {
+            navMeshAgent.enabled = !health.IsDead();
+
+            UpdateAnimator();
+        }
+
+        public void StartMoveAction(Vector3 destination)
+        {            
+            GetComponent<ActionScheduler>().StartAction(this);
+            MoveTo(destination);
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = navMeshAgent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+
+        public void Cancel()
+        {
+            navMeshAgent.isStopped = true;
+        }
+
+
+        public void MoveTo(Vector3 destination)
+        {
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+    }
+}
+

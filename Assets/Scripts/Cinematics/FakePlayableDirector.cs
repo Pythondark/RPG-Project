2c175fc64214fa86f138b3c307a041c9b4d398de
jacoblightspeedsolutions <jacob@lightspeedsolutions.ca>
2019-11-03 15:31:22 -0800

    Created Sandbox 2 Scene

---
+++ Assets/Scripts/Cinematics/FakePlayableDirector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakePlayableDirector : MonoBehaviour
+{
+
+    public event Action<float> onFinish;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("OnFinish", 3f);
+    }
+
+    void OnFinish()
+    {
+        onFinish(34.0f);
+    }
+}

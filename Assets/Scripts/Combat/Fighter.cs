3af86f6af6a53769b362915ed39821f2ddfbd3f4
jacoblightspeedsolutions <jacob@lightspeedsolutions.ca>
2019-11-11 16:53:19 -0800

    Added Weapon scriptable objects. Added ability to pickup a weapon.

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -8,22 +8,19 @@ namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour, IAction
     {
-        [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttacks = 1f;
-        [SerializeField] float weaponDamage = 5f;
         [SerializeField] Transform handTransform = null;
-        [SerializeField] GameObject weaponPrefab = null;
+        [SerializeField] Weapon defaultWeapon = null;
 
         Health target;
         float timeSinceLastAttack = Mathf.Infinity;
+        Weapon currentWeapon = null;
 
 
         private void Start()
         {
-            SpawnWeapon();
+            EquipWeapon(defaultWeapon);
         }
-
-
 
         private void Update()
         {
@@ -45,12 +42,11 @@ namespace RPG.Combat
             }
         }
 
-        private void SpawnWeapon()
+        public void EquipWeapon(Weapon weapon)
         {
-            if (weaponPrefab != null)
-            {
-                Instantiate(weaponPrefab, handTransform);
-            }
+            currentWeapon = weapon;
+            Animator animator = GetComponent<Animator>();
+            currentWeapon.Spawn(handTransform, animator);          
         }
 
         public bool CanAttack(GameObject combatTarget)
@@ -89,14 +85,14 @@ namespace RPG.Combat
         void Hit()
         {
             if (target == null) { return; }
-            target.TakeDamage(weaponDamage);
+            target.TakeDamage(currentWeapon.GetDamage());
         }
 
 
 
         private bool GetIsInRange()
         {
-            return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
+            return Vector3.Distance(transform.position, target.transform.position) < currentWeapon.GetRange();
         }
 
 
---
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class WeaponPickup : MonoBehaviour
+    {
+        [SerializeField] Weapon weapon = null;
+
+        private void OnTriggerEnter(Collider otherCollider)
+        {
+            if (otherCollider.gameObject.tag == "Player")
+            {
+                otherCollider.GetComponent<Fighter>().EquipWeapon(weapon);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
+

dce9d260418e96bb9412d1298acb319f385087c8
jacoblightspeedsolutions <jacob@lightspeedsolutions.ca>
2019-11-12 21:54:27 -0800

    Added projectile abilities to Weapon. Bows now shoot arrows.

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using RPG.Core;
+using UnityEngine;
 
 namespace RPG.Combat
 {
@@ -11,22 +12,41 @@ namespace RPG.Combat
         [SerializeField] float weaponDamage = 5f;
         [SerializeField] bool isRightHanded = true;
         [SerializeField] bool isPickaxe = false;
+        [SerializeField] Projectile projectile = null;
 
         public void Spawn(Transform rightHand, Transform leftHand, Animator animator)
         {
             if (equippedPrefab != null)
             {
-                Transform handTransform;
-                if (isRightHanded) handTransform = rightHand;
-                else handTransform = leftHand;
-                Instantiate(equippedPrefab, handTransform);                
+                Transform handTransform = GetTransform(rightHand, leftHand);
+                Instantiate(equippedPrefab, handTransform);
             }
 
 
             if (animatorOverride != null)
             {
                 animator.runtimeAnimatorController = animatorOverride;
-            }                        
+            }
+
+        }
+
+        private Transform GetTransform(Transform rightHand, Transform leftHand)
+        {
+            Transform handTransform;
+            if (isRightHanded) handTransform = rightHand;
+            else handTransform = leftHand;
+            return handTransform;
+        }
+
+        public bool HasProjectile()
+        {
+            return projectile != null;
+        }
+
+        public void LaunchProjectile(Transform rightHand, Transform leftHand, Health target)
+        {
+            Projectile projectileInstance = Instantiate(projectile, GetTransform(rightHand, leftHand).position, Quaternion.identity);
+            projectileInstance.SetTarget(target);
         }
 
         public float GetRange()
